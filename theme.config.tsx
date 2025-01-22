9f5f3537589eeded38c5c16eed39ef0f486ce8f8
jonnydeppe <162109640+jonnydeppe@users.noreply.github.com>
2025-01-22 09:05:14 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Lunul Foundation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/lunulfoundation',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/lunulfoundation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Lunul Foundation',
   },
 }
 
